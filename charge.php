09a4607c488d98bd71345aec80e75a84c194c6df
Xinpeng Sun <sunxinpeng1994@gmail.com>
2019-12-13 10:46:15 -0600

    stripe page

---
+++ charge.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("./stripe/init.php");
+    echo "submitted";
+    \Stripe\Stripe::setApiKey('sk_test_MRKcfgQ1KJztFjh4fLEsIpZx00Hs3hcPTk');
+
+
+    $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+    $email = $POST['email'];
+    $token = $POST['stripeToken'];
+
+    echo $token;
+
+
+    //create customer in stripe
+    $customer = \Stripe\Customer::create(array(
+        "email" => $email,
+        "source" => $token
+    ));
+    //charge the customer
+    $charge = \Stripe\Charge::create(array(
+        "amount" => 1500,
+        "currency" => "usd",
+        "description" => "membership fee",
+        "customer" => $customer->id
+    ));
+    print_r($charge);
+    // $intent = \Stripe\PaymentIntent::create([
+    //     'amount' => 1099,
+    //     'currency' => 'usd',
+    // ]);
--- payment.php
+++ payment.php
@@ -2,83 +2,138 @@
 <html>
 <head>
   <title>Checkout</title>
-  <script src="https://js.stripe.com/v3/"></script>
-  <script>
-    var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
-    var elements = stripe.elements();
-    var style = {
-        base: {
-            color: "#32325d",
-        }
-    };
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+  <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+   
+<style>
+/**
+ * The CSS shown here will not be introduced in the Quickstart guide, but shows
+ * how you can use CSS to style your Element's container.
+ */
+.StripeElement {
+  box-sizing: border-box;
 
-    var card = elements.create("card", { style: style });
-    card.mount("#card-element");
+  height: 40px;
 
+  padding: 10px 12px;
 
+  border: 1px solid transparent;
+  border-radius: 4px;
+  background-color: white;
 
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
 
-    card.addEventListener('change', function(event) {
-        var displayError = document.getElementById('card-errors');
-        if (event.error) {
-            displayError.textContent = event.error.message;
-        } else {
-            displayError.textContent = '';
-        }
-    });
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
 
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
 
-
-
-    var submitButton = document.getElementById('submit');
-
-    submitButton.addEventListener('click', function(ev) {
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {card: card}
-        }).then(function(result) {
-            if (result.error) {
-            // Show error to your customer (e.g., insufficient funds)
-            console.log(result.error.message);
-            } else {
-            // The payment has been processed!
-            if (result.paymentIntent.status === 'succeeded') {
-                console.log("works");
-                // Show a success message to your customer
-                // There's a risk of the customer closing the window before callback
-                // execution. Set up a webhook or plugin to listen for the
-                // payment_intent.succeeded event that handles any business critical
-                // post-payment actions.
-            }
-            }
-        });
-    });
-  </script>
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+</style>
 </head>
 
 <body>
 
-<?php
-require_once('./stripe/init.php');
+<div class="container">
+    <h2 class="my-4 text-center">Membership Fee $1500</h2>
+    <form action="./charge.php" method="post" id="payment-form">
+        <div class="form-row">
+            <input type="text" placeholder="Email" name="email" class="form-control mb-3 StripeElement StripeELement--empty">
+            <div id="card-element" class="form-control">
+            <!-- A Stripe Element will be inserted here. -->
+            </div>
 
-\Stripe\Stripe::setApiKey('sk_test_MRKcfgQ1KJztFjh4fLEsIpZx00Hs3hcPTk');
+            <!-- Used to display form errors. -->
+            <div id="card-errors" role="alert"></div>
+        </div>
 
-$intent = \Stripe\PaymentIntent::create([
-    'amount' => 1099,
-    'currency' => 'usd',
-]);
-
-
-
-?>
-
-<div id="card-element">
-  <!-- Elements will create input elements here -->
+        <button class="btn btn-primary btn-block mt-4">Submit Payment</button>
+    </form>
 </div>
 
-<!-- We'll put the error messages in this element -->
-<div id="card-errors" role="alert"></div>
+<script src="https://js.stripe.com/v3/"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        
+<script>
+    // Create a Stripe client.
+var stripe = Stripe('pk_test_d3N4EXBcqefgMpFXoLELBfbK00xC8vwDVB');
 
-<button id="submit">Pay</button>
+// Create an instance of Elements.
+var elements = stripe.elements();
 
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+// Submit the form with the token ID.
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  // Submit the form
+  form.submit();
+}
+  </script>
 </body>
 </html>
